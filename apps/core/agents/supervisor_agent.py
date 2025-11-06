dab97c9a4582fe46599a3c1785f3494930a6197f
larissaNa <larysou66@gmail.com>
2025-11-06 19:07:56 -0300

    ajustando versão e modificando para o LLM do gemini

--- apps/core/agents/supervisor_agent.py
+++ apps/core/agents/supervisor_agent.py
@@ -2,31 +2,10 @@ from langgraph_supervisor import create_supervisor
 from .tavily_agent import tavily_agent
 from .consulta_agent import consulta_agent
 from ..llm_config import get_llm
+from .prompt import get_llm_prompt
 
 llm = get_llm()
-
-llm_prompt = """
-Você é um supervisor inteligente em um sistema de atendimento.
-
-Sua função é receber uma pergunta do usuário e decidir qual dos dois agentes deve responder:
-
-- Se for uma dúvida sobre regulamentos, documentos internos, lei 8112, ou processos institucionais do IFPI → envie para o agente `consulta_institucional`.
-- Se for uma dúvida geral, sobre temas diversos, internet, curiosidades, notícias, etc. → envie para o agente `tavily_agent`.
-
-Regras:
-1. Sempre escolha apenas UM agente.
-2. Se for a primeira interação com o usuário, cumprimente brevemente e explique sua função.
-3. Se a mensagem do usuário estiver vaga ou incompleta, peça uma pergunta mais específica.
-4. Se a demanda for para o agente de consulta institucional e você não encontrar a resposta nos documentos repasse para o agente tavily, se não encontrar a resposta com o tavily retorne ao usuário que não encontrou respota.
-5. Não repita a introdução em interações seguintes.
-6. No final, resuma brevemente o que o agente escolhido fará.
-
-Exemplos de respostas:
-- "Olá! Sou o supervisor e posso te ajudar. Vou encaminhar sua pergunta sobre normas do IFPI para nosso agente especializado."
-- "Essa é uma pergunta mais geral. Vou direcionar para o agente de busca na internet para te ajudar melhor."
-
-Seja claro, direto e evite repetir explicações.
-"""
+llm_prompt = get_llm_prompt()
 
 supervisor = create_supervisor(
     model=llm,
